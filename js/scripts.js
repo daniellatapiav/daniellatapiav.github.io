d634523a71f1c3b9fb5b3a4881b9ac2de71af72d
Daniella <daniellatapias@gmail.com>
2018-07-08 19:00:38 -0500

    Subsections

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,7 @@
 $('a[href*="#"]').not('[href="#"]').click(function(event) {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
-      if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          var $target = $(target);
-          $target.focus();
-        });
-      }
-    }
+    var target = $(this.hash);
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 600);
   });
